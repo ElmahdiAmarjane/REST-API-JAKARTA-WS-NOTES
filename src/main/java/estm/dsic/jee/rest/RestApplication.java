b863f63bc34cf9d8baa6a053f19e7fc8c62bbe20
amarjane <amarjaneelmahdi03@gmail.com>
2024-03-01 14:18:59 +0100

    Initialize project

---
+++ src/main/java/estm/dsic/jee/rest/RestApplication.java
@@ -0,0 +1,15 @@
+package estm.dsic.jee.rest;
+
+
+import javax.print.attribute.standard.MediaName;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+
+@ApplicationPath("/api/gnotes/v1")
+public class RestApplication extends Application {
+  
+}
